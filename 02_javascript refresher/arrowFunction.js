554f41b3976cf0871db4ebcb10eda4d033d6db79
harsha-ys <hyoshith@gmail.com>
2023-02-05 11:49:37 +0530

    arrow functions

---
+++ 02_javascript refresher/arrowFunction.js
@@ -0,0 +1,20 @@
+const name = "Yoshith";
+let age = 27;
+
+const userr = (name, age)=>
+{
+    return(
+        "name is " + name + " age is "+ age
+    )
+}
+
+console.log(userr(name, age));
+
+const add = (a, b) => a+b;
+console.log(add(3, 5));
+
+const addrandom = () => 1+2;
+console.log(addrandom());
+
+const oneadd= a=> a +1;
+console.log(oneadd(6));
---
+++ 02_javascript refresher/play.js
@@ -0,0 +1,2 @@
+var name = "Yoshith";
+console.log(name);
