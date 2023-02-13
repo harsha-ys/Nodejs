421cc51b12b4f86c85aaebe97885cb906c594799
harsha-ys <hyoshith@gmail.com>
2023-02-13 23:01:32 +0530

    stop the server process using exit() system call

--- 03_understanding the basics/app.js
+++ 03_understanding the basics/app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res)=>{
     console.log("request is caught");
     console.log(req);
+    process.exit();// stop the server
 });
 
 server.listen(3000);
