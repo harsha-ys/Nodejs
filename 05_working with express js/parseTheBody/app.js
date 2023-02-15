5e3d37c1ec866c31b5f3b6c7da04dce6970840de
harsha-ys <hyoshith@gmail.com>
2023-02-15 19:10:22 +0530

    redirecting and body parsing using express and body-parser

--- 05_working with express js/parseTheBody/app.js
+++ 05_working with express js/parseTheBody/app.js
@@ -2,19 +2,24 @@
 //send html file in which a form is existed and after submit it will does a post request to the serever
 //post request is sent to /message api and it will redirect it to the localhost
 //read the data from the post request
+// npm install --save body-parser
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 app = express();
 
-app.use("/", (req, res, next)=>{
-    console.log("first Middleware and alwaays run");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use("/add-product", (req, res, next)=>{
+    console.log("add-product Middleware");
+    res.send('<form action = "/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
 });
 
-app.use("/app-product", (req, res, next)=>{
-    console.log("app-product Middleware");
-    res.send("<h1>Hello This is from app-product Midlleware</h1>");
+app.use("/product", (req, res, next)=>{
+    console.log("product Middleware");
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/", (req, res, next)=>{
