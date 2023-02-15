e9d5b9c75537b179b5b6e54ebe9680397eb674a5
harsha-ys <hyoshith@gmail.com>
2023-02-15 16:03:10 +0530

    first middleware

--- 05_working with express js/app1/app.js
+++ 05_working with express js/app1/app.js
@@ -4,9 +4,12 @@
 //read the data from the post request
 const http = require("http");
 
-const hd = require('./routes');
+const app = require("express");
 
-console.log(hd.text);
+app.use((req, res, next)=>{
+    console.log("first Middleware");
+});
+
 const server = http.createServer(hd.handler);
 
 server.listen(3000);
