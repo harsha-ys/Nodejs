88182a85f44017aa7daa7a22a9401688e0f69d86
harsha-ys <hyoshith@gmail.com>
2023-02-16 00:13:34 +0530

    filtering paths

--- 05_working with express js/expressRoutes/app.js
+++ 05_working with express js/expressRoutes/app.js
@@ -17,7 +17,7 @@ const shopRouter = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(adminRouter);
+app.use('/admin',adminRouter);//filtering paths
 app.use(shopRouter);
 app.use((req, res, next)=>{
     res.status(404).send('<h1>Page not Found</h1>');
