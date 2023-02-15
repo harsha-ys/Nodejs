d62f86453e0ccf424f67f6a0534dd63cd581923f
harsha-ys <hyoshith@gmail.com>
2023-02-15 23:17:55 +0530

    send  page not found for all undefined endpoints

--- 05_working with express js/expressRoutes/app.js
+++ 05_working with express js/expressRoutes/app.js
@@ -19,7 +19,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(adminRouter);
 app.use(shopRouter);
-
 app.use((req, res, next)=>{
     res.status(404).send('<h1>Page not Found</h1>');
 });
