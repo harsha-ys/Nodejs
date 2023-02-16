1cee3d404c1b086b7d9543d635596f57a06b8e6e
harsha-ys <hyoshith@gmail.com>
2023-02-16 13:48:08 +0530

    add two views and send those html views using path module

--- 05_working with express js/with view(html)/routes/admin.js
+++ 05_working with express js/with view(html)/routes/admin.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const adminRouter = express.Router();
 
 // /admin/add-product => get
 adminRouter.get("/add-product", (req, res, next)=>{ //.get() match the url exactly
     console.log("add-product Middleware");
-    res.send('<form action = "/admin/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
+    //res.send('<form action = "/admin/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
+    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 // /admin/add-product => post
--- 05_working with express js/with view(html)/routes/shop.js
+++ 05_working with express js/with view(html)/routes/shop.js
@@ -1,11 +1,13 @@
 const express =  require("express");
+const path = require("path");
 
 const shopRouter = express.Router();
 
 
 shopRouter.get("/", (req, res, next)=>{
     console.log("last Middleware");
-    res.send("<h1>Hello This is from Express Midlleware</h1>");
+    res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+    //res.send("<h1>Hello This is from Express Midlleware</h1>");
 });
 
 
