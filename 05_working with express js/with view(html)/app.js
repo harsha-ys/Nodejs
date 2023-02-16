c448d0f0f1da2e515361b090e716d397fd6eb3a5
harsha-ys <hyoshith@gmail.com>
2023-02-16 14:32:53 +0530

    add page not found view

--- 05_working with express js/with view(html)/app.js
+++ 05_working with express js/with view(html)/app.js
@@ -10,6 +10,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -20,7 +21,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin',adminRouter);//filtering paths
 app.use(shopRouter);
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
