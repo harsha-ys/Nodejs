597ea0c66b3500c3f624ebfc033e18aad762d992
harsha-ys <hyoshith@gmail.com>
2023-02-17 17:58:44 +0530

    print product titles in console

--- 06_working with dynamic content Adding templating engines/server 1/routes/shop.js
+++ 06_working with dynamic content Adding templating engines/server 1/routes/shop.js
@@ -1,12 +1,14 @@
 const express =  require("express");
 const path = require("path");
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const shopRouter = express.Router();
 
 
 shopRouter.get("/", (req, res, next)=>{
-    console.log("last Middleware");
+    console.log("shop Middleware");
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, "views", "shop.html"));
     //res.send("<h1>Hello This is from Express Midlleware</h1>");
 });
