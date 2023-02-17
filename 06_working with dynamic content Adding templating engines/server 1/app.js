555031995200cc3c9917518b9753823b5a4ab1c7
harsha-ys <hyoshith@gmail.com>
2023-02-17 16:23:17 +0530

    start the server

---
+++ 06_working with dynamic content Adding templating engines/server 1/app.js
@@ -0,0 +1,30 @@
+// sending a response for the request
+//send html file in which a form is existed and after submit it will does a post request to the serever
+//post request is sent to /message api and it will redirect it to the localhost
+//read the data from the post request
+// npm install --save body-parser
+
+//"restart": true,
+//"runtimeExecutable": "nodemon",
+//"console": "integratedTerminal"   in configuration file
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+const rootDir = require("./util/path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/admin',adminRouter);//filtering paths
+app.use(shopRouter);
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+app.listen(3000);
---
+++ 06_working with dynamic content Adding templating engines/server 1/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
+
+const adminRouter = express.Router();
+
+// /admin/add-product => get
+adminRouter.get("/add-product", (req, res, next)=>{ //.get() match the url exactly
+    console.log("add-product Middleware");
+    //res.send('<form action = "/admin/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+// /admin/add-product => post
+adminRouter.post("/product", (req, res, next)=>{ // using app.post, only triggered for post requests
+    console.log("product Middleware");
+    console.log(req.body);
+    res.redirect("/");
+});
+
+module.exports = adminRouter
