8625123990ff21bbcaf21b4641ba90863fea568a
harsha-ys <hyoshith@gmail.com>
2023-02-17 17:59:56 +0530

    make changes as we change admin.js by exporting another variable

--- 06_working with dynamic content Adding templating engines/server 1/app.js
+++ 06_working with dynamic content Adding templating engines/server 1/app.js
@@ -12,7 +12,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminRouter = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 const rootDir = require("./util/path");
 
@@ -21,7 +21,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin',adminRouter);//filtering paths
+app.use('/admin', adminData.routes);//filtering paths
 app.use(shopRouter);
 app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
