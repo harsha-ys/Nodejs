008aba21994d63de03f3994bf75cc48be78f6a50
harsha-ys <hyoshith@gmail.com>
2023-03-05 21:15:34 +0530

    render 404 page not found page by passing the title to the pug file

--- 06_working with dynamic content Adding templating engines/server 1/app.js
+++ 06_working with dynamic content Adding templating engines/server 1/app.js
@@ -31,7 +31,7 @@ app.use('/admin', adminData.routes);//filtering paths
 app.use(shopRouter);
 app.use((req, res, next)=>{
     //res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-    res.status(404).render('404');
+    res.status(404).render('404', {title: 'Page Not Found'});
 });
 
 app.listen(3000);
