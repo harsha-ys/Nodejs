cc7a20c5002bf660d6e5474dbeb4041939efb629
harsha-ys <hyoshith@gmail.com>
2023-03-05 20:12:11 +0530

    render add-product pug file

--- 06_working with dynamic content Adding templating engines/server 1/routes/admin.js
+++ 06_working with dynamic content Adding templating engines/server 1/routes/admin.js
@@ -8,7 +8,8 @@ const adminRouter = express.Router();
 adminRouter.get("/add-product", (req, res, next)=>{ //.get() match the url exactly
     console.log("add-product Middleware");
     //res.send('<form action = "/admin/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
-    res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    res.render('add-product' , {title : 'Add Product'});
 });
 
 
