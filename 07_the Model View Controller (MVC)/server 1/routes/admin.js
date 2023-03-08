1c2996bb78c7e2b1ac42b5b058f1180491f064df
harsha-ys <hyoshith@gmail.com>
2023-03-08 17:11:06 +0530

    use controllers from routes

--- 07_the Model View Controller (MVC)/server 1/routes/admin.js
+++ 07_the Model View Controller (MVC)/server 1/routes/admin.js
@@ -6,15 +6,7 @@ const adminRouter = express.Router();
 // /admin/add-product => get
 adminRouter.get("/add-product", productControllers.getAddProduct);
 
-
-const products = [];
 // /admin/add-product => post
-adminRouter.post("/add-product", (req, res, next)=>{ // using app.post, only triggered for post requests
-    console.log("product Middleware");
-    //console.log(req.body);
-    products.push({title: req.body.title});
-    res.redirect("/");
-});
+adminRouter.post("/add-product", productControllers.postAddProduct);
 
 module.exports.routes = adminRouter
-module.exports.products = products
--- 07_the Model View Controller (MVC)/server 1/routes/shop.js
+++ 07_the Model View Controller (MVC)/server 1/routes/shop.js
@@ -1,28 +1,10 @@
 const express =  require("express");
-const path = require("path");
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productControllers = require('../controllers/product');
 
 const shopRouter = express.Router();
 
 
-shopRouter.get("/", (req, res, next)=>{
-    console.log("shop Middleware");
-    console.log(adminData.products);
-    const products = adminData.products;
-    //res.sendFile(path.join(rootDir, "views", "shop.html"));
-    //res.send("<h1>Hello This is from Express Midlleware</h1>");
-    res.render('shop', 
-    {
-        prods:products, 
-        title :"Shop", 
-        act:'shop', 
-        hasitem : products.length,
-        activeShop: true,
-        activeAddProduct: false,
-        productCSS: true        
-    });
-});
+shopRouter.get("/", productControllers.getShop);
 
 
 module.exports = shopRouter;
