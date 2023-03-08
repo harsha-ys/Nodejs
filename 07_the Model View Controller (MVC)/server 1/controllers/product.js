7d7be62f4f52dbc82c29537f7b50da8a0c926a44
harsha-ys <hyoshith@gmail.com>
2023-03-08 17:10:46 +0530

    product controller

--- 07_the Model View Controller (MVC)/server 1/controllers/product.js
+++ 07_the Model View Controller (MVC)/server 1/controllers/product.js
@@ -12,3 +12,29 @@ module.exports.getAddProduct = (req, res, next)=>{ //.get() match the url exactl
         formsCSS: true
     });
 }
+
+const products = [];
+module.exports.postAddProduct=(req, res, next)=>{ // using app.post, only triggered for post requests
+    console.log("product Middleware");
+    //console.log(req.body);
+    products.push({title: req.body.title});
+    res.redirect("/");
+}
+
+
+module.exports.getShop=(req, res, next)=>{
+    console.log("shop Middleware");
+    console.log(products);
+    //res.sendFile(path.join(rootDir, "views", "shop.html"));
+    //res.send("<h1>Hello This is from Express Midlleware</h1>");
+    res.render('shop', 
+    {
+        prods:products, 
+        title :"Shop", 
+        act:'shop', 
+        hasitem : products.length,
+        activeShop: true,
+        activeAddProduct: false,
+        productCSS: true        
+    });
+}
