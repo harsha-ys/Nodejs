e348ca0282d989cbb048e52cb872cd6e860b6399
harsha-ys <hyoshith@gmail.com>
2023-03-10 04:38:43 +0530

    use callbacks to correct teh issue of returning after all the functions in teh  main thread are execution due to teh asynchronous readFile function, remove the repeated code using teh callbacks as well

--- 07_the Model View Controller (MVC)/server 1/controllers/product.js
+++ 07_the Model View Controller (MVC)/server 1/controllers/product.js
@@ -1,7 +1,5 @@
 module.exports.getAddProduct = (req, res, next)=>{ //.get() match the url exactly
     console.log("add-product Middleware");
-    //res.send('<form action = "/admin/product" method = "POST" ><input type="text" name="product"><button type="submit">ADD this product</button></form>');
-    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
     res.render('add-product' , 
     {
         title : 'Add Product', 
@@ -16,11 +14,9 @@ module.exports.getAddProduct = (req, res, next)=>{ //.get() match the url exactl
 
 
 const product = require('../models/products');
-//const products = [];
+
 module.exports.postAddProduct=(req, res, next)=>{ // using app.post, only triggered for post requests
     console.log("product Middleware");
-    //console.log(req.body);
-    //products.push({title: req.body.title});
     const newproduct = new product(req.body.title);
     newproduct.add();
     res.redirect("/");
@@ -29,17 +25,16 @@ module.exports.postAddProduct=(req, res, next)=>{ // using app.post, only trigge
 
 module.exports.getShop=(req, res, next)=>{
     console.log("shop Middleware");
-    console.log(product.fetchAll());
-    //res.sendFile(path.join(rootDir, "views", "shop.html"));
-    //res.send("<h1>Hello This is from Express Midlleware</h1>");
-    res.render('shop', 
-    {
-        prods: product.fetchAll(), 
-        title :"Shop", 
-        act:'shop', 
-        hasitem : product.fetchAll().length,
-        activeShop: true,
-        activeAddProduct: false,
-        productCSS: true        
+    product.fetchAll((products)=>{
+        res.render('shop', 
+        {
+            prods: products, 
+            title :"Shop", 
+            act:'shop', 
+            activeShop: true,
+            activeAddProduct: false,
+            productCSS: true        
+        });
     });
+    
 }
