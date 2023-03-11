bd36be803fd581bb64e8088fc45be4e560a9435e
harsha-ys <hyoshith@gmail.com>
2023-03-11 18:08:35 +0530

    troubleshooting

--- 08_enhancing the app/Registerring the routes/routes/admin.js
+++ 08_enhancing the app/Registerring the routes/routes/admin.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const productControllers = require('../controllers/product');
+const adminControllers = require('../controllers/product');
 
 const adminRouter = express.Router();
 
 // /admin/add-product => get
-adminRouter.get("/add-product", productControllers.getAddProduct);
+adminRouter.get("/add-product", adminControllers.getAddProduct);
+
+// /admin/products => get
+adminRouter.get("/products", adminControllers.getProducts);
 
 // /admin/add-product => post
-adminRouter.post("/add-product", productControllers.postAddProduct);
+adminRouter.post("/add-product", adminControllers.postAddProduct);
 
 module.exports.routes = adminRouter
