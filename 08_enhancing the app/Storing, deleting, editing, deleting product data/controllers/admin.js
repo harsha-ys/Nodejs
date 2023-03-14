d0117062ec8ded4a462c934e180d03711a7779c2
harsha-ys <hyoshith@gmail.com>
2023-03-15 02:44:18 +0530

    fetch products for admin

--- 08_enhancing the app/Storing, deleting, editing, deleting product data/controllers/admin.js
+++ 08_enhancing the app/Storing, deleting, editing, deleting product data/controllers/admin.js
@@ -10,19 +10,25 @@ module.exports.getAddProduct = (req, res, next)=>{ //.get() match the url exactl
     });
 }
 
+const product = require('../models/products');
+
 module.exports.getProducts = (req, res, next)=>{ //.get() match the url exactly
     console.log("add-product Middleware");
-    res.render('admin/product-list' , 
-    {
-        title : 'Products', 
-        act : 'add-product',
-        path: '/admin/products',
-        productCSS: true,
-        formsCSS: true
+    product.fetchAll(products=>{
+        res.render('admin/product-list' , 
+        {
+            prods: products,
+            title : 'Products', 
+            act : 'add-product',
+            path: '/admin/products',
+            productCSS: true,
+            formsCSS: true
+        });     
     });
+
 }
 
-const product = require('../models/products');
+
 
 module.exports.postAddProduct=(req, res, next)=>{ // using app.post, only triggered for post requests
     console.log("product Middleware");
